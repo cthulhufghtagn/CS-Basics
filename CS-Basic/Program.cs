82dfc7963e6bbd4a153fe6a5353497ef8b7bf610
pc161 <pc161@DESKTOP-P22KOIM>
2021-11-17 16:52:01 +0200

    Добавьте файлы проекта.

---
+++ CS-Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;
+int b;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+int sum = a + b;
+int dif = a - b;
+int mult = a * b;
+float div = (float) a / b;
+
+Console.WriteLine("sum: " + sum + " dif: " + dif + " mult: " + mult + " div: " + div);
